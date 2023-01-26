9480e953b5936ff70e1dc61d0489abcb1bdc9783
ThomasThings <89316672+ThomasThings@users.noreply.github.com>
2023-01-26 15:19:26 -0500

    mod.java

---
+++ src/erekir-scaled/ErekirScaledMod.java
@@ -0,0 +1,13 @@
+package erekir-scaled;
+
+import mindustry.mod.*;
+import erekir-scaled.content.*;
+
+public class ErekirScaledMod extends Mod{
+
+    @Override
+
+    public void loadContent(){
+    }
+
+}
