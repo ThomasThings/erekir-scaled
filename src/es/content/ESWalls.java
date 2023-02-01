e6dae3bbb5ebbb7f35e8144a3ff03cca3c9c727c
ThomasThings <89316672+ThomasThings@users.noreply.github.com>
2023-02-01 10:45:31 -0500

    walls

--- src/es/content/ESWalls.java
+++ src/es/content/ESWalls.java
@@ -5,14 +5,19 @@ import mindustry.world.*;
 import mindustry.world.blocks.defense.*;
 import mindustry.content.Blocks;
 import mindustry.content.Items;
+import mindustry.world.meta.Env;
 
 import static mindustry.type.ItemStack.*;
 
 public class ESWalls{
     public static Block
             berylliumWallHuge, berylliumWallGigantic,
+            reinforcedGraphiteWall, reinforcedGraphiteWallLarge, reinforcedGraphiteWallHuge, reinforcedGraphiteWallGigantic,
+            reinforcedSiliconWall, reinforcedSiliconWallLarge, reinforcedSiliconWallHuge, reinforcedSiliconWallGigantic,
             tungstenWallHuge, tungstenWallGigantic,
+            oxideWall, oxideWallLarge, oxideWallHuge, oxideWallGigantic,
             blastGate, blastDoorLarge, blastDoorHuge,
+            reinforcedThoriumWall, reinforcedThoriumWallLarge, reinforcedThoriumWallHuge, reinforcedThoriumWallGigantic,
             reinforcedSurgeWallHuge, reinforcedSurgeWallGigantic,
             carbideWallHuge, carbideWallGigantic,
             shieldedWallSmall, shieldedWallLarge, shieldedWallHuge;
@@ -34,6 +39,78 @@ public class ESWalls{
             buildCostMultiplier = 2f;
             size = 4;
         }};
+        reinforcedGraphiteWall = new Wall("reinforced-graphite-wall"){{
+            requirements(Category.defense, with(Items.graphite, 5, Items.beryllium, 2));
+            health = 150 * wallHealthMultiplier;
+            armor = 5f;
+            buildCostMultiplier = 8f;
+            insulated = true;
+            schematicPriority = 10;
+        }};
+        reinforcedGraphiteWallLarge = new Wall("reinforced-graphite-wall-large"){{
+            requirements(Category.defense, with(Items.graphite, 20, Items.beryllium, 8));
+            health = 150 * wallHealthMultiplier * 4;
+            armor = 5f;
+            buildCostMultiplier = 5f;
+            insulated = true;
+            schematicPriority = 10;
+        }};
+        reinforcedGraphiteWallHuge = new Wall("reinforced-graphite-wall-huge"){{
+            requirements(Category.defense, with(Items.graphite, 45, Items.beryllium, 18));
+            health = 150 * wallHealthMultiplier * 9;
+            armor = 5f;
+            buildCostMultiplier = 3f;
+            insulated = true;
+            schematicPriority = 10;
+        }};
+        reinforcedGraphiteWallGigantic = new Wall("reinforced-graphite-wall-gigantic"){{
+            requirements(Category.defense, with(Items.graphite, 80, Items.beryllium, 32));
+            health = 150 * wallHealthMultiplier * 16;
+            armor = 5f;
+            buildCostMultiplier = 2f;
+            insulated = true;
+            schematicPriority = 10;
+        }};
+        reinforcedSiliconWall = new Wall("reinforced-silicon-wall"){{
+            requirements(Category.defense, with(Items.silicon, 5, Items.graphite, 2));
+            health = 150 * wallHealthMultiplier;
+            armor = 5f;
+            buildCostMultiplier = 8f;
+            insulated = true;
+            schematicPriority = 10;
+            lightningChance = 0.05f;
+            lightningDamage = 5f;
+        }};
+        reinforcedSiliconWallLarge = new Wall("reinforced-silicon-wall-large"){{
+            requirements(Category.defense, with(Items.silicon, 20, Items.graphite, 8));
+            health = 150 * wallHealthMultiplier * 4;
+            armor = 5f;
+            buildCostMultiplier = 5f;
+            insulated = true;
+            schematicPriority = 10;
+            lightningChance = 0.05f;
+            lightningDamage = 5f;
+        }};
+        reinforcedSiliconWallHuge = new Wall("reinforced-silicon-wall-huge"){{
+            requirements(Category.defense, with(Items.silicon, 45, Items.graphite, 18));
+            health = 150 * wallHealthMultiplier * 9;
+            armor = 5f;
+            buildCostMultiplier = 3f;
+            insulated = true;
+            schematicPriority = 10;
+            lightningChance = 0.025f;
+            lightningDamage = 10f;
+        }};
+        reinforcedSiliconWallGigantic = new Wall("reinforced-silicon-wall-gigantic"){{
+            requirements(Category.defense, with(Items.silicon, 80, Items.graphite, 32));
+            health = 150 * wallHealthMultiplier * 16;
+            armor = 5f;
+            buildCostMultiplier = 2f;
+            insulated = true;
+            schematicPriority = 10;
+            lightningChance = 0.025f;
+            lightningDamage = 10f;
+        }};
         tungstenWallHuge = new Wall("tungsten-wall-huge"){{
             requirements(Category.defense, ItemStack.with(Items.tungsten, 54));
             health = 180 * wallHealthMultiplier * 9;
@@ -45,6 +122,33 @@ public class ESWalls{
             requirements(Category.defense, ItemStack.with(Items.tungsten, 96));
             health = 180 * wallHealthMultiplier * 16;
             armor = 14f;
+            buildCostMultiplier = 2f;
+            size = 4;
+        }};
+        oxideWall = new Wall("oxide-wall"){{
+            requirements(Category.defense, ItemStack.with(Items.oxide, 5, Items.beryllium, 2));
+            health = 200 * wallHealthMultiplier;
+            armor = 13f;
+            buildCostMultiplier = 8f;
+        }};
+        oxideWallLarge = new Wall("oxide-wall-large"){{
+            requirements(Category.defense, ItemStack.with(Items.oxide, 20, Items.beryllium, 8));
+            health = 200 * wallHealthMultiplier * 4;
+            armor = 13f;
+            buildCostMultiplier = 5f;
+            size = 2;
+        }};
+        oxideWallHuge = new Wall("oxide-wall-huge"){{
+            requirements(Category.defense, ItemStack.with(Items.oxide, 45, Items.beryllium, 18));
+            health = 200 * wallHealthMultiplier * 9;
+            armor = 13f;
+            buildCostMultiplier = 3f;
+            size = 3;
+        }};
+        oxideWallGigantic = new Wall("oxide-wall-gigantic"){{
+            requirements(Category.defense, ItemStack.with(Items.oxide, 80, Items.beryllium, 32));
+            health = 200 * wallHealthMultiplier * 16;
+            armor = 13f;
             buildCostMultiplier = 2f;
             size = 4;
         }};
@@ -63,6 +167,29 @@ public class ESWalls{
             requirements(Category.defense, with(Items.tungsten, 96, Items.silicon, 96));
             health = 175 * wallHealthMultiplier * 9;
             armor = 14f;
+            size = 4;
+        }};
+        reinforcedThoriumWall = new Wall("reinforced-thorium-wall"){{
+            requirements(Category.defense, ItemStack.with(Items.thorium, 6, Items.tungsten, 2));
+            health = 230 * wallHealthMultiplier;
+            armor = 20f;
+        }};
+        reinforcedThoriumWallLarge = new Wall("reinforced-thorium-wall-large"){{
+            requirements(Category.defense, ItemStack.with(Items.thorium, 24, Items.tungsten, 8));
+            health = 230 * wallHealthMultiplier * 4;
+            armor = 20f;
+            size = 2;
+        }};
+        reinforcedThoriumWallHuge = new Wall("reinforced-thorium-wall-huge"){{
+            requirements(Category.defense, ItemStack.with(Items.thorium, 54, Items.tungsten, 18));
+            health = 230 * wallHealthMultiplier * 9;
+            armor = 20f;
+            size = 3;
+        }};
+        reinforcedThoriumWallGigantic = new Wall("reinforced-thorium-wall-gigantic"){{
+            requirements(Category.defense, ItemStack.with(Items.thorium, 96, Items.tungsten, 32));
+            health = 230 * wallHealthMultiplier * 16;
+            armor = 20f;
             size = 4;
         }};
         reinforcedSurgeWallHuge = new Wall("reinforced-surge-wall-huge"){{
